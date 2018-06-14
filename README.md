40d57966091ee4164235b9b878667545dbeaa6f5
Christian Tschoepe <tschoepe.christian@gmail.com>
2018-06-14 11:13:18 +0300

    Initial project commit and README

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # react-key-gen
 A Natural Solution for React list Keys
+
+```bash
+npm install react-key-gen
+```
+
+```node
+import ReactKeyGen from 'react-key-gen';
+
+class MyComponent extends React.Component {
+    state = {
+        items: [],
+        keyGen: new ReactKeyGen(),
+    }
+
+    addItemToList = (item) => {
+        this.setState({
+            items: [
+                ...this.state.items,
+                this.state.keyGen.keyedItem(item),
+            ]
+        });
+    }
+
+    render() {
+        return (
+            <ul>
+                {this.state.items.map(item => (
+                    <li key={item._key}>{item.title}</li>
+                ))}
+            </ul>
+        );
+    }
+}
+```
---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * Instances of ReactKeyGen will generate keys for each item that
+ * does not naturally have unique keys. This can be useful to
+ * naturally solve React Key errors as well as for identity
+ * of objects over time and over property changes.
+ */
+export default class ReactKeyGen {
+    keyNo = 1;
+  
+    /**
+     * Generate a `_key` property on `item`
+     * that can be used for React element list keys.
+     *
+     * Note that `{ ...item }._key` is `undefined`.
+     * For copying items with keys, use `@copyItem`.
+     */
+    keyedItem = (item) => {
+      // If item has a `_key`, it's good to go
+      if (item._key) {
+        return item;
+      }
+  
+      // Generate a unique `_key` prop that cannot be enumerated.
+      Object.defineProperty(item, '_key', {
+        value: this.keyNo,
+        enumerable: false,
+      });
+      this.keyNo += 1;
+      return item;
+    };
+  
+    /**
+     * Shallow copy an item with its unenumerable key.
+     */
+    copyItem = (item) => {
+      const newItem = { ...item };
+      Object.defineProperty(item, '_key', {
+        value: item._key,
+        enumerable: false,
+      });
+      return newItem;
+    };
+  }
+  
